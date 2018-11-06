92342261972dd94d75931bc469755afb1545a54b
Wan Cheng Cheng <chengcheng.st@gmail.com>
2018-11-07 02:38:40 +0800

    Add ava test configs.

---
+++ ava.config.js
@@ -0,0 +1,9 @@
+/*
+ * File: ava.config.js
+ * File Created: Wednesday, 7th November 2018 2:34:23 am
+ * Author: ChegCheng Wan (chengcheng.st@gmail.com)
+ */
+
+export default {
+  sources: ['**/*.test.js', '**/*.e2e.js', '!build/**/*'],
+};
